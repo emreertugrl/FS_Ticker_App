39d9810946c5c61a08555e6a34f79b326f72dd98
emreertugrl <emrertugrl@gmail.com>
2025-01-23 22:13:14 +0300

    connect mongodb function defined to project, ticket control function defined to api route

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -1,7 +1,37 @@
+import connectMongo from "@/app/utils/connect-mongo";
 import { NextResponse } from "next/server";
+import Ticket from "./models/Ticket";
 
-export async function GET() {
-  return NextResponse.json({
-    message: "Hello from Next.js API!",
-  });
+export async function GET(): Promise<void> {
+  try {
+    await connectMongo();
+    // API endpoint for getting data from MongoDB
+    const tickets = await Ticket.find();
+    NextResponse.json({
+      message: "Ticket verileri alındı.",
+      tickets,
+    });
+    return;
+  } catch (error) {
+    NextResponse.json({
+      message: "Ticket verileri alınırken bir hata oluştu",
+      error: error,
+    });
+    return;
+  }
+}
+
+export async function POST(): Promise<void> {
+  try {
+    await connectMongo();
+    // isteğin body kısmındaki veriyi al
+
+    // veritabanına yeni ticket'ı kaydet
+
+    // client'e cevap döndür
+    NextResponse.json({
+      message: "Hello from Next.js API!",
+    });
+    return;
+  } catch (error) {}
 }
