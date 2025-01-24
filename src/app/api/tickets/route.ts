8e13395085308db23df8b7469315ea9e81b0e11c
emreertugrl <emrertugrl@gmail.com>
2025-01-24 20:45:08 +0300

    tickets get and post controller defined to api

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -2,36 +2,49 @@ import connectMongo from "@/app/utils/connect-mongo";
 import { NextResponse } from "next/server";
 import Ticket from "./models/Ticket";
 
-export async function GET(): Promise<void> {
+export async function GET(): Promise<NextResponse> {
   try {
     await connectMongo();
     // API endpoint for getting data from MongoDB
     const tickets = await Ticket.find();
-    NextResponse.json({
+    return NextResponse.json({
       message: "Ticket verileri alındı.",
       tickets,
     });
-    return;
   } catch (error) {
-    NextResponse.json({
-      message: "Ticket verileri alınırken bir hata oluştu",
+    return NextResponse.json({
+      message: "Ticket verileri alınırken bir hata oluştu!!!",
       error: error,
     });
-    return;
   }
 }
 
-export async function POST(): Promise<void> {
+export async function POST(req: Request): Promise<NextResponse> {
   try {
     await connectMongo();
     // isteğin body kısmındaki veriyi al
+    const body = await req.json();
 
     // veritabanına yeni ticket'ı kaydet
+    // const newTicket = await Ticket.create(body);
+    const newTicket = new Ticket(body);
+    await newTicket.save();
 
     // client'e cevap döndür
-    NextResponse.json({
-      message: "Hello from Next.js API!",
-    });
-    return;
-  } catch (error) {}
+    return NextResponse.json(
+      {
+        message: "Ticket Oluşturuldu",
+        ticket: newTicket,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Ticket oluşturulma başarısız!!!",
+        error: error,
+      },
+      { status: 400 }
+    );
+  }
 }
