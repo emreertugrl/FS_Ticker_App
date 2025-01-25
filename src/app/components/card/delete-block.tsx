d1d5f41e101208c159da95050860f7262a7493ca
emreertugrl <emrertugrl@gmail.com>
2025-01-25 17:50:20 +0300

    delete card design completed

--- src/app/components/card/delete-block.tsx
+++ src/app/components/card/delete-block.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { FaTrash } from "react-icons/fa";
 type Props = {
   id: string;
 };
 const DeleteBlock = ({ id }: Props) => {
-  return <div>DeleteBlock</div>;
+  return <FaTrash className="cursor-pointer hover:text-red-500 transition" />;
 };
 
 export default DeleteBlock;
