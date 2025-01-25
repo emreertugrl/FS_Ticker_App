f6fc90b3a205f1ffe50ad18b42ce41628a84446a
emreertugrl <emrertugrl@gmail.com>
2025-01-25 17:53:33 +0300

    progress design completed

--- src/app/components/card/progress-block.tsx
+++ src/app/components/card/progress-block.tsx
@@ -3,7 +3,18 @@ type Props = {
   progress: number;
 };
 const ProgressBlock = ({ progress }: Props) => {
-  return <div>ProgressBlock</div>;
+  return (
+    <div className="w-full h-2.5 bg-gray-200 rounded-full">
+      <div
+        className="h-2.5 rounded-full"
+        style={{
+          width: `${progress}%`,
+          backgroundColor: "#0084DE",
+          transition: "width 0.5s ease-in-out",
+        }}
+      ></div>
+    </div>
+  );
 };
 
 export default ProgressBlock;
