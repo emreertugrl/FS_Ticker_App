47eefbdd0694c99b72d91a6473b2884969b33929
emreertugrl <emrertugrl@gmail.com>
2025-01-25 18:17:41 +0300

    status design completed

--- src/app/components/card/status-block.tsx
+++ src/app/components/card/status-block.tsx
@@ -2,7 +2,23 @@ type Props = {
   status: string;
 };
 const StatusBlock = ({ status }: Props) => {
-  return <div>StatusBlock</div>;
+  const getColor = () => {
+    switch (status.toLowerCase()) {
+      case "beklemede":
+        return "bg-orange-500";
+      case "devam ediyor":
+        return "bg-blue-500";
+      case "çözüldü":
+        return "bg-green-500";
+      default:
+        return "bg-gray-500";
+    }
+  };
+  return (
+    <span className={`inline-block rounded-full px-2 py-1 font-semibold text-xs ${getColor()}`}>
+      {status}
+    </span>
+  );
 };
 
 export default StatusBlock;
